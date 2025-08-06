c42f1756a34ece573def2cabc0cb87f5f2ee2c08
Alanxzg <alangamarro0@gmail.com>
2025-08-05 21:38:45 -0600

    Update insertar_Crud.php

--- insertar_Crud.php
+++ insertar_Crud.php
@@ -1,16 +1,19 @@
 <?php
-include ("connection.php");
+include("connection.php");
 
- $con = connection();
-$id = null;
-$name = $_POST ['name' ];
-$lastname = $_POST ['lastname' ];
-$username = $_POST ['username' ];
-$password = $_POST ['password' ];
-$email = $_POST ['email' ];
-$sql = "INSERT INTO persona VALUES ('$id ', '$name', '$lastname ','$username','$password' ,'$email ' )";
-$query = mysqli_query($con,$sql);
-if ($query){
-    header ("Location: index.php");
+$con = connection();
+
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO persona (name, apellido, usuario, clave, email) 
+        VALUES ('$name', '$lastname', '$username', '$password', '$email')";
+$query = mysqli_query($con, $sql);
+
+if ($query) {
+    header("Location: index.php");
 }
 ?>
