e9c5c2eab73288bdfbdba932f07f11a16172c17d
Alanxzg <alangamarro0@gmail.com>
2025-08-05 21:38:13 -0600

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,21 +1,13 @@
 <?php
 
-function connection (){
+function connection() {
     $host = "localhost";
     $user = "root";
     $pass = "";
     $db = "crud1";
 
-    //medoto
+    $connect = mysqli_connect($host, $user, $pass, $db);
 
-    $connect = mysqli_connect($host,$user,$pass, $db  );
-    
-    mysqli_select_db($connect,$db );
-
-    return $connect ;
-
-
+    return $connect;
 }
-
-
 ?>
